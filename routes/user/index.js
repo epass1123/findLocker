38d9b93aa145264cb3cd16d142688c3a666a750c
epass1123 <404.notfound@kakao.com>
2023-04-25 16:17:07 +0900

    fix(mypage):added conditional statement in case of session expired

--- routes/user/index.js
+++ routes/user/index.js
@@ -25,16 +25,22 @@ router.post('/',
     async (req,res)=>{
         const { password } = req.body;
         try{
-            let id = req.session.user.id
-            let user = await User.findOne({ id });
-            const check = await bcrypt.compare(password, user.password);
-          if(check){
-            return res.redirect("../user/myinfo/modify")
-          }
-          else{
-            return res.
-            send(alertMove("../user/index", "비밀번호가 다릅니다."))
-          }
+            if(req.session.user){
+                let id = req.session.user.id
+                let user = await User.findOne({ id });
+                const check = await bcrypt.compare(password, user.password);
+              if(check){
+                return res.redirect("../user/myinfo/modify")
+              }
+              else{
+                return res.
+                send(alertMove("../user/index", "비밀번호가 다릅니다."))
+              }
+            }
+            else{
+                return res.
+                send(alertMove("/routes/login","로그인이 필요합니다."))
+            }
         }
         catch(e){
             console.log(e)
