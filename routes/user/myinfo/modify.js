f82c357824080e9128bedce8de3e14a537513ad7
epass1123 <404.notfound@kakao.com>
2023-04-26 15:46:46 +0900

    feat: added mongoose update when routes/user/myinfo/modify sent post

--- routes/user/myinfo/modify.js
+++ routes/user/myinfo/modify.js
@@ -1,5 +1,7 @@
 import express from "express";
 import alertMove from "/Users/kim/Desktop/web/memosite/js/util/alertMove.js";
+import User from '../../../models/users.js';
+import bcrypt from 'bcrypt';
 const router = express.Router();
 
 router.get('/',
@@ -20,8 +22,42 @@ router.get('/',
 )
 
 router.post('/',
-    async (req,res)=>{
-        
+    async (req,res)=>{ 
+        let _id = req.session.user._id
+        const { name, id ,email, password, passwordChk } = req.body;
+        try{
+            if(req.session.user){
+                if(password === passwordChk){
+                    console.log("같음")
+                    const salt = await bcrypt.genSalt(10);
+                    let encryptPassword = await bcrypt.hash(password, salt);
+                    await User.updateMany({_id:_id},
+                        {
+                            $set:{
+                                name:name,
+                                id:id,
+                                email:email,
+                                password:encryptPassword
+                            }
+                        }
+                    )
+                    let user = await User.findOne({ _id });
+                    req.session.user = user;
+                    return res.
+                        send(alertMove("../index","회원정보 수정 완료"))
+                    }
+                else{
+                    console.log("비밀번호다름")
+                }
+                    
+            }
+            else{
+                console.log("해당하는 유저 없음")
+            }
+        }
+        catch(e){
+            console.log(e)
+        }
     }
 )
 
